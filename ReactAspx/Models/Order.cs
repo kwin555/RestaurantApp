4cdb21db64cdc630ecf3620b9dd2d9239d6a6870
kwin555 <ktn555@att.net>
2019-07-07 17:21:33 -0500

    Add project files.

---
+++ ReactAspx/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReactAspx.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalPaid { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int status { get; set; }
+    }
+}
